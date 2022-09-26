9990480aefb9bddf1803b1af23b319c44a2705ac
Ansen123 <ansenfp@gmail.com>
2022-09-26 16:18:07 +0530

    dfghjk

---
+++ src/com/nest/calcu/Calcu.java
@@ -0,0 +1,30 @@
+package com.nest.calcu;
+import java.util.Scanner;
+public class Calcu {
+    public static void main(String[] args) {
+        Scanner op= new Scanner(System.in);
+        int choice;
+        int num1, num2, num3;
+
+        System.out.println( "Enter the two numbers");
+
+        num1= op.nextInt();
+        num2= op.nextInt();
+        choice= op.nextInt();
+        switch(choice){
+            case 1:
+                System.out.println(num3=num1+num2);
+                break;
+            case 2:
+                System.out.println(num3=num1-num2);
+                break;
+            case 3:
+                System.out.println(num1*num2);
+                break;
+            case 4:
+                System.out.println(num1/num2);
+                break;
+
+        }
+    }
+}
